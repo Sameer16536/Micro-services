29aaa74d51c01612c5520b440cef908906884271
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2024-12-28 22:58:14 +0530

    Add user login functionality to user.controller.js

--- users/controllers/user.controller.js
+++ users/controllers/user.controller.js
@@ -25,3 +25,24 @@ module.exports.register = async(req,res)=>{
         res.status(500).json({error:error})
     }
 } 
+
+module.exports.login = async(req,res)=>{
+    try{
+        const{email,password}  = req.body
+        const user = await userModel.findOne({email}).select('+password')
+        
+        if(!user){
+            res.status(400).json({message:"Invalid Credentials"})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            res.status(400).json({message:"Invalid password"})
+        }
+
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'1h'})
+        
+    }catch(error){
+        res.status('404').json({error:error})
+    }
+}
