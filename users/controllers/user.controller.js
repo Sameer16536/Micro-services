c333fa9bb51f8560f1db976abf4cec6b13f4fc07
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2024-12-29 18:14:06 +0530

    Enhance user authentication: include token in registration response and add logout functionality to user.controller.js

--- users/controllers/user.controller.js
+++ users/controllers/user.controller.js
@@ -19,7 +19,8 @@ module.exports.register = async(req,res)=>{
 
         const token = jwt.sign({id:newUser._id},process.env.JWT_SECRET,{expiresIn:'3h'})
         res.cookie('token',token)
-        res.send({message:"User registered successfully"})
+        delete newUser._doc.password
+        res.send({token,newUser,message:"User registered successfully"})
     }
     catch(error){
         res.status(500).json({error:error})
@@ -44,5 +45,16 @@ module.exports.login = async(req,res)=>{
         
     }catch(error){
         res.status('404').json({error:error})
+    }
+
+    module.exports.logout = async (req, res) => {
+        try {
+            const token = req.cookies.token;
+            await blacklisttokenModel.create({ token });
+            res.clearCookie('token');
+            res.send({ message: 'User logged out successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
     }
 }
